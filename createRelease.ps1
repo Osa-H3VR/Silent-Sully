282254ad9885525e49c3d9d943d6f942161cab0a
OsaPL <osa12pl@gmail.com>
2021-04-03 01:47:01 +0200

    adding release script

---
+++ createRelease.ps1
@@ -0,0 +1,26 @@
+﻿$manifest = Get-Content .\manifest.json | ConvertFrom-Json
+$deliName = $manifest.name+$manifest.version+".deli"
+$deliZipName = $manifest.name+$manifest.version+".zip"
+$zipName = "UnZipMePls-"+$manifest.name+$manifest.version+".zip"
+
+#cleanup if anything is left behind
+if (Test-Path $deliName) {
+  Remove-Item $deliName
+}
+if (Test-Path $deliZipName) {
+  Remove-Item $deliZipName
+}
+if (Test-Path $zipName) {
+  Remove-Item $zipName
+}
+
+#create deli mod
+Compress-Archive $manifest.name $deliZipName
+Compress-Archive -Update "manifest.json" $deliZipName
+Rename-Item $deliZipName $deliName
+
+#create release package
+Compress-Archive $deliName $zipName
+Compress-Archive -Update "README.md" $zipName
+
+Remove-Item $deliName
